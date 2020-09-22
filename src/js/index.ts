18648f1861d6f3c5d735d3133ad8129bf3c69b4f
zentarg <apg.chili@gmail.com>
2020-09-22 12:47:22 +0200

    All tasks done. Forgot to commit for each task.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,41 @@
 //Write your Typescript code here
+
+
+let listOfItems : HTMLCollectionOf<HTMLLIElement> = <HTMLCollectionOf<HTMLLIElement>> document.getElementsByTagName("li");
+
+console.log(listOfItems);
+
+for (let index = 0; index < listOfItems.length; index++) {
+    const element = listOfItems[index];
+    if (element.classList.contains("unhealthy"))
+        element.setAttribute("class", "healthy");
+     else if (element.classList.contains("healthy"))
+        element.setAttribute("class", "unhealthy");
+}
+
+let itemsList : HTMLUListElement = <HTMLUListElement> listOfItems[0].parentElement;
+
+itemsList.firstElementChild.setAttribute("style", "color: blue");
+itemsList.lastElementChild.setAttribute("style", "color: black");
+
+let newItem : HTMLLIElement = <HTMLLIElement> document.createElement("li");
+newItem.innerText = "Pizza";
+newItem.id = "4";
+newItem.classList.add("unhealthy");
+
+itemsList.appendChild(newItem);
+
+let unhealthyList : HTMLCollectionOf<HTMLLIElement> = <HTMLCollectionOf<HTMLLIElement>> document.getElementsByClassName("unhealthy");
+
+let unhealthyItemsList : HTMLUListElement = <HTMLUListElement> document.getElementById("listUnhealthy");
+
+for (let index = unhealthyList.length; index > 0; index--) {
+    const element = unhealthyList[index - 1];
+    element.parentNode.removeChild(element);
+    unhealthyItemsList.appendChild(element);
+}
+
+let marshmellowsLI : HTMLLIElement = <HTMLLIElement> document.getElementById("0");
+marshmellowsLI.innerText = "Bananas";
+
+unhealthyItemsList.removeChild(unhealthyItemsList.firstElementChild);
